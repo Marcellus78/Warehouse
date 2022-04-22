756b739f2749a98774cd151d96cbb18d55f406ff
Maciek Moniuszko <moniuszko@gmail.com>
2022-04-22 14:36:56 +0200

    Adding code behind main window (not working)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Magazyn.Context;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 
 namespace Magazyn
 {
@@ -20,9 +11,39 @@ namespace Magazyn
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private readonly ToolContext context = new ToolContext();
+        private CollectionViewSource categoryViewSource;
         public MainWindow()
         {
             InitializeComponent();
+            categoryViewSource =
+                (CollectionViewSource)FindResource(nameof(categoryViewSource));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            context.Database.EnsureCreated();
+            context.Categories.Load();
+            categoryViewSource.Source =
+                context.Categories.Local.ToObservableCollection();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // all changes are automatically tracked, including
+            // deletes!
+            context.SaveChanges();
+
+            // this forces the grid to refresh to latest values
+            categoryDataGrid.Items.Refresh();
+            toolsDataGrid.Items.Refresh();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // clean up database connections
+            context.Dispose();
+            base.OnClosing(e);
         }
     }
 }
